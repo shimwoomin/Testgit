8e30d56439c983c087d6e403777085268419d087
swm97 <swm97@naver.com>
2020-01-15 09:42:36 +0900

    add e

--- MemberDTO.java
+++ MemberDTO.java
@@ -1,53 +1,96 @@
 ﻿package com.naver;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.io.Serializable;
 
-public class MemberDAO {
+public class MemberDTO implements Serializable{
 
-	public static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
-	public static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-	public static final String USER = "ca2";
-	public static final String PASSWORD = "ca2";
-
-	public MemberDAO() {
-		try {
-			Class.forName(DRIVER);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private int age;
+	
+	public MemberDTO() {
+		// TODO Auto-generated constructor stub
 	}
 
-	public void insert(MemberDTO dto) {
-
-		Connection conn = null;
-		PreparedStatement pt = null;
-		String sql = "insert into member2(id,name,age)values(?,?,?)";
-
-		try {
-			conn = DriverManager.getConnection(URL,USER,PASSWORD);
-			pt = conn.prepareStatement(sql);
-			pt.setString(1, dto.getId());
-			pt.setString(2, dto.getName());
-			pt.setInt(3, dto.getAge());
-			pt.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (pt != null) {
-					pt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-		}
+	public MemberDTO(String id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
 	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MemberDTO other = (MemberDTO) obj;
+		if (age != other.age)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "MemberDTO [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+	
 
 }
+
